96931859fdccf05988500dd9f651af5f9b0a3c97
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-04-19 21:04:18 +0900

    initial commit

---
+++ src/test/java/blue/berry/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package blue.berry.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
